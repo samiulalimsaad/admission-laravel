a55f54acb00b40fc9356fb1272d7e583835cc302
samiulalimsaad <samiulalimsaad@gmail.com>
2024-11-09 20:11:10 +0600

    main layout added

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Admission Test</title>
+    <title>
+        {{ $attributes['title'] }} - Admission</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -15,25 +16,13 @@
 
 </head>
 
-<body class="font-sans antialiased " data-theme='light'>
-    <div class="bg-gray-50 text-black/50 ">
+<body class="font-sans" data-theme='light'>
+    @include('components.navbar')
+    <div class="container mx-auto">
 
-        <button class="btn btn-primary">Click me</button>
+        {{ $slot }}
 
-        <!-- Open the modal using ID.showModal() method -->
-        <button class="btn" onclick="my_modal_1.showModal()">open modal</button>
-        <dialog id="my_modal_1" class="modal">
-            <div class="modal-box">
-                <h3 class="text-lg font-bold">Hello!</h3>
-                <p class="py-4">Press ESC key or click the button below to close</p>
-                <div class="modal-action">
-                    <form method="dialog">
-                        <!-- if there is a button in form, it will close the modal -->
-                        <button class="btn">Close</button>
-                    </form>
-                </div>
-            </div>
-        </dialog>
+
     </div>
 </body>
 
