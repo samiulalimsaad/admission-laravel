44a5e95300ac95b55a76e070dc2806e80c7c3ce7
samiulalimsaad <samiulalimsaad@gmail.com>
2024-11-09 20:35:26 +0600

    home added

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+<x-layout title="Home">
+    <div class="card w-1/2 mx-auto bg-white py-8">
+        <!-- Logo and Title Section -->
+        <div class="flex flex-col items-center mb-4">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/c/c0/Rajshahi_University_update_logo.png"
+                alt="University of Rajshahi Logo" class="mb-2 h-32">
+            <h1 class="text-2xl font-semibold text-gray-800">University of Rajshahi</h1>
+            <h2 class="text-xl font-medium text-gray-700">Undergraduate Admission 2022-23</h2>
+        </div>
+
+        <!-- Information Links -->
+        <div class="card-body">
+            <ul class="list-disc m-8 space-y-3">
+                <li><a href="#" class="link">Conditions for Different Units</a></li>
+                <li><a href="#" class="link">Conditions for Different Quota</a></li>
+                <li><a href="#" class="link">Exam Routine</a></li>
+                <li><a href="#" class="link">Preliminary Application Guideline</a></li>
+                <li><a href="#" class="link">Application form for Ward Quota</a></li>
+            </ul>
+        </div>
+
+        <div class="card-actions justify-center">
+            <a class="btn btn-primary" href='{{ route('info') }}'>Preliminary Application</a>
+        </div>
+    </div>
+
+
+</x-layout>
