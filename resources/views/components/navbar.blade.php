c9d8d1e868abc28941d9508703bcfdd5fba659fa
samiulalimsaad <samiulalimsaad@gmail.com>
2024-11-09 20:11:03 +0600

    navbar added

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,14 @@
+<nav class="bg-base-100">
+    <div class="navbar container mx-auto">
+        <div class="navbar-start">
+            <a class="btn btn-ghost text-xl items-center flex">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/c/c0/Rajshahi_University_update_logo.png"
+                    alt="University of Rajshahi Logo" class="h-10">
+                <h1 class="text-2xl font-semibold text-gray-800">University of Rajshahi</h1>
+            </a>
+        </div>
+        <div class="navbar-end">
+            <a class="btn btn-primary">Login</a>
+        </div>
+    </div>
+</nav>
